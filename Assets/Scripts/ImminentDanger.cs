8d46c34d89977a431299626cf418f5b8e729e5d1
LOUGRADA Nassim <n.lougrada@rubika-edu.com>
2019-11-19 15:58:47 +0100

    Imminent Danger

---
+++ Assets/Scripts/ImminentDanger.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ImminentDanger : MonoBehaviour
+{
+
+    public GlitchEffect effect;
+    public float radius;
+
+    private float ratio;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ratio = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ratio = 1f;
+        Collider[] overlaped = Physics.OverlapSphere(transform.position, radius);
+        foreach(Collider c in overlaped)
+        {
+            if ((c.transform.position - transform.position).magnitude < ratio)
+            {
+                ratio = (c.transform.position - transform.position).magnitude;
+            }
+        }
+        effect.intensity = 1 - ratio;
+        effect.flipIntensity = 1 - ratio;
+        effect.colorIntensity = 1 - ratio;
+    }
+}
---
+++ Assets/Scripts/Tests/SpookyShader.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpookyShader : MonoBehaviour
+{
+
+    public Material spookyMat;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        Graphics.Blit(source, null, spookyMat);
+    }
+}
