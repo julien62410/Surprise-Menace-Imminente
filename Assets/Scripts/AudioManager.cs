b9853433d9eecc447ae9005686a929f13c22d40a
LOUGRADA Nassim <n.lougrada@rubika-edu.com>
2019-11-19 16:39:28 +0100

    Audio

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+
+    public float delay;
+
+    private AudioSource source;
+    private float timer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+        timer = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer >= delay)
+        {
+            if (source.volume >= 0.1f)
+            {
+                source.Play();
+                timer = 0f;
+            }
+        }
+    }
+}
