3932c4af75f817f5f1ed51a35116084f213fd088
Betrancourt Valentin <valentin.betrancourt@hotmail.fr>
2019-11-21 11:09:34 +0100

    Surfaces detector / shadows / menu

--- Assets/Scripts/DetectSurfaces.cs
+++ Assets/Scripts/DetectSurfaces.cs
@@ -6,23 +6,72 @@ using GoogleARCore;
 public class DetectSurfaces : MonoBehaviour
 {
     public GameObject plane;
-    public List<DetectedPlane> detectedPlanes = new List<DetectedPlane>();
-
-    public bool isInit = false;
+    public List<DetectedPlane> m_detectedPlanes = new List<DetectedPlane>();
+    public List<GameObject> listPlaneInScene = new List<GameObject>();
 
     private void Update()
     {
-        Session.GetTrackables<DetectedPlane>(detectedPlanes, TrackableQueryFilter.All);
+        CheckLostTracking();
 
-        if (detectedPlanes.Count > 0)
+        if (Session.Status != SessionStatus.Tracking)
         {
-            for (int i = 0; i < detectedPlanes.Count; i++)
-            {
-                Pose pose = detectedPlanes[i].CenterPose;
-                GameObject _plane = Instantiate(plane, pose.position, pose.rotation);
-                _plane.transform.localScale = new Vector3(detectedPlanes[i].ExtentX / 10, detectedPlanes[i].ExtentX / 10, detectedPlanes[i].ExtentZ / 10);
-            }
+            return;
         }
 
+        foreach (GameObject plane in listPlaneInScene)
+        {
+            plane.SetActive(true);
+        }
+
+        Session.GetTrackables<DetectedPlane>(m_detectedPlanes, TrackableQueryFilter.New);
+
+        for (int i = 0; i < m_detectedPlanes.Count; i++)
+        {
+            DisplayPlane(m_detectedPlanes[i]);
+
+        }
+    }
+
+    private void DisplayPlane(DetectedPlane m_detectedPlane)
+    {
+        Pose pose = m_detectedPlane.CenterPose;
+        Anchor anchor = m_detectedPlane.CreateAnchor(pose);
+
+        GameObject planeObject =
+            Instantiate(plane, anchor.transform.position, anchor.transform.rotation);
+
+        listPlaneInScene.Add(planeObject);
+        planeObject.transform.localScale = new Vector3(m_detectedPlane.ExtentX / 10, m_detectedPlane.ExtentX / 10, m_detectedPlane.ExtentZ / 10);
+
+        planeObject.transform.SetParent(anchor.transform);
+    }
+
+    private void CheckLostTracking() // SOURCE : GOOGLE HELLO AR
+    {
+        if (Session.Status == SessionStatus.LostTracking &&
+            Session.LostTrackingReason != LostTrackingReason.None)
+        {
+            foreach (GameObject plane in listPlaneInScene)
+            {
+                plane.SetActive(false);
+            }
+
+            switch (Session.LostTrackingReason)
+            {
+                case LostTrackingReason.InsufficientLight:
+                    Debug.Log("Too dark. Try moving to a well-lit area.");
+                    break;
+                case LostTrackingReason.InsufficientFeatures:
+                    Debug.Log("Aim device at a surface with more texture or color.");
+                    break;
+                case LostTrackingReason.ExcessiveMotion:
+                    Debug.Log("Moving too fast. Slow down.");
+                    break;
+                default:
+                    Debug.Log("Motion tracking is lost.");
+                    break;
+            }
+            return;
+        }
     }
 }
